3b33354bf18bafcf6dd6062549ba58599a37303c
manojd929 <manojthetechguy@gmail.com>
2024-07-02 13:25:28 +0530

    add icons to services and short description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import BackgroundComp from "@/components/BackgroundComp";
 import config from '@/config/config.json';
 import { Button, Card, CardContent, CardHeader, Grid, Typography } from "@mui/material";
@@ -41,10 +42,11 @@ export default function Home() {
           <Grid container spacing={6}>
             {services.map((svc, index) => (
               <Grid key={svc.name + index} item xs={12} md={4} lg={4} >
-                <Card variant="outlined">
-                  <CardHeader title={svc.name} />
-                  <CardContent>
-                    {svc.description}
+                <Card variant="outlined" className={styles.serviceCard}>
+                  <Image className={styles.serviceIcon} src={svc.icon} height={48} width={48} alt={svc.name + ' icon'}/>
+                  <CardHeader title={svc.name} className={styles.serviceCardHeader}/>
+                  <CardContent className={styles.serviceCardContent}>
+                    {svc.short_desc}
                   </CardContent>
                 </Card>
               </Grid>
